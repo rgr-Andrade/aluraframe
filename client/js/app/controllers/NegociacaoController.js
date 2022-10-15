bb65da11b157b1a1ec069c417e84d141504625b8
Rogério Andrade <rgrandrade@hotmail.com>
2022-10-15 14:59:30 -0300

    Criação das classes DateHelper, ListaNegociacoes e uso do Templete string

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,31 +7,30 @@ class NegociacaoController{
 		this._inputData = $('#data');
         this._inputQuantidade =  $('#quantidade');
        	this._inputValor = $('#valor');
+       	this._listaNegociacoes = new ListaNegociacoes();
 
 	}
 
 
-	adiciona(event){
-		
-		event.preventDefault();
+	adiciona(event) {
 
-		let data = new Date(...this._inputData
-            .value.split('-')
-            .map((item, indice)=> item - indice % 2)
+        event.preventDefault();
+		this._listaNegociacoes.adiciona(this._criaNegociacao());
+	    this._limpaFormulario();
+	}
 
-        );
+	_criaNegociacao(){
+		return new Negociacao(
+	        DateHelper.textoParaData(this._inputData.value),
+	        this._inputQuantidade.value,
+	        this._inputValor.value);
+	}
 
-		let negociacao = new Negociacao(
-    	
-    		data,
-    		this._inputQuantidade.value,
-    		this._inputValor.value
-
-  		);
-
-  console.log(negociacao);
-
-
-        
+	_limpaFormulario(){
+		this._inputData.value = '';
+		this._inputQuantidade.value = 1;
+		this._inputValor = 0.0;
+		this._inputData.focus(); 
 	}
 }
+ 
