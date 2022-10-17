953e82bae05dd9fdbd11deaee0bafc4815818aad
Rogério Andrade <rgrandrade@hotmail.com>
2022-10-16 21:55:19 -0300

    Criação da classe de Mensagem e trabalhando com Herança

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,11 +1,11 @@
-class NegociacoesView {
+class NegociacoesView extends View{
     
-    constructor(elemento) {
-        
-        this._elemento = elemento;
-    }
+   constructor(elemento){
+
+		super(elemento);
+	}
     
-    _template(model) {
+    template(model) {
         
         return `
         <table class="table table-hover table-bordered">
@@ -42,8 +42,5 @@ class NegociacoesView {
         `;
     }
     
-    update(model) {
-        
-        this._elemento.innerHTML = this._template(model);
-    }
+    
 }
